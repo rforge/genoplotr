e32ec07b79612f8c4d77923e4fd316751d458465
lionelguy <lionelguy@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-01-25 15:24:49 +0000

    Added a link and a better text on index.php
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/genoplotr@8 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -12,27 +12,30 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
   
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title><?php echo $group_name; ?></title>
-    <link href="css/main.css" rel="stylesheet" type="text/css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title><?php echo $group_name; ?></title>
+  <link href="css/main.css" rel="stylesheet" type="text/css" />
   </head>
   
   <body>
   <!-- includes table and top row and colum -->
   <?php include("begin.php"); ?>  
-    <h1>genoPlotR</h1>
-    <p>
-    <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-      $contents = '';
-      while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-      }
-      fclose($handle);
-      echo $contents; } ?>
-    </p>
-    <p>The <strong>project summary page</strong> you can find 
-      <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">
-      <strong>here</strong></a>. 
-    </p>
+  <h1>genoPlotR</h1>
+  <p>
+  <?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+  $contents = '';
+  while (!feof($handle)) {
+    $contents .= fread($handle, 8192);
+  }
+  fclose($handle);
+  echo $contents; } ?>
+  </p>
+  <p>This project is hosted on <a href="http://r-forge.r-project.org/">R-Forge</a>.
+  <p>The <strong>project summary page</strong> can be found
+  <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">
+  <strong>here</strong></a>, with all info on downloading, obtaining help, reporting
+  bugs and proposing features.
+  </p>
   <!-- includes end of table --> 
   <?php include("end.php"); ?>
+  
--- www/leftmenu.php
+++ www/leftmenu.php
@@ -1,8 +1,15 @@
+<?php
+$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
+$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
+?>
 <!--left menu-->
 <p>&nbsp;</p>
 <p><a href="index.php">Home</a>
 </p>
 <p><a href="screenshots.php">Screenshots</a></br>
+</p>
+<p><a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/">
+  R-Forge summary page</a></br>
 </p>
 <!--/left menu-->
    
